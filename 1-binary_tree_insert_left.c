f65ddc85046eeefadca402039ec622e74fa55300
haythmelmagraby <haythmelmagraby@gmail.com>
2024-04-09 05:28:15 -0700

    1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -7,18 +7,20 @@
  *
  *Return: a pointer to the new node, or NULL on failure
  */
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent,__attribute__((unused))  int value)
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *bt;
-	if(!parent)
+
+	if (!parent)
 		return (NULL);
 
-	bt = binary_tree_node(parent,value);
+	bt = binary_tree_node(parent, value);
 
 	if (bt == NULL)
 		return (NULL);
 
-	if (parent->left != NULL){
+	if (parent->left != NULL)
+	{
 		bt->left = parent->left;
 		parent->left->parent = bt;
 	}
