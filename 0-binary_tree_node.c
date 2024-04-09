4e66c013bb8eb8e9645c20f8dcbd695ee15e68f6
haythmelmagraby <haythmelmagraby@gmail.com>
2024-04-09 04:52:46 -0700

    0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -12,14 +12,15 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 
 	binary_tree_t *bt;
+
 	bt = malloc(sizeof(binary_tree_t));
 
-	if(!bt)
-		return NULL;
+	if (!bt)
+		return (NULL);
 
 	bt->parent = parent;
 	bt->n = value;
 	bt->left = NULL;
 	bt->right = NULL;
-	return(bt);
+	return (bt);
 }
