61c4535a999a9fabc4e3c4f685b184771b9f817b
haythmelmagraby <haythmelmagraby@gmail.com>
2024-04-11 05:41:59 -0700

    9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,6 +10,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree)
 	{
 		size_t left = 0, right = 0;
+
 		if (tree->left)
 			left = binary_tree_height(tree->left) + 1;
 		if (tree->right)
